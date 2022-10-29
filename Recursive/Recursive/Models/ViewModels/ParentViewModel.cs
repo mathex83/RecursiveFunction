be8e338d1a68bfaab22090f0452e1d20d6e5ee02
Martin <marnor83@gmail.com>
2022-10-29 13:32:50 +0200

    added viewmodelclasses

---
+++ Recursive/Recursive/Models/ViewModels/ParentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Recursive.Models.ViewModels;
+
+public class ParentViewModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<ViewModel> SubList { get; set; } = new();
+}
---
+++ Recursive/Recursive/Models/ViewModels/ViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Recursive.Models.ViewModels;
+
+public class ViewModel
+{
+    public int Id { get; set; }
+    public int? Pid { get; set; }
+    public string Name { get; set; }
+    public List<ViewModel> SubList { get; set; } = new();
+}
