a344d8671cd457697bcd618bbe611e84471653a6
Martin <marnor83@gmail.com>
2022-10-29 13:32:32 +0200

    Added modelclass and programclass

---
+++ Recursive/Recursive/Models/CSharpModels/Model.cs
@@ -0,0 +1,59 @@
+﻿namespace Recursive.Models.CSharpModels;
+
+public class Model
+{
+    public int Id { get; set; }
+    public int? ParentId { get; set; }
+    public string Name { get; set; }
+    public List<Model> Children { get; set; } = new();
+    
+    public static List<Model> CreateModelList()
+    {
+        List<Model> virtualDbList = new();
+        int id = 0;
+        for (int i = 1; i < 3; i++)
+        {
+            id++;
+            Model w = new()
+            {
+                Id = id,
+                Name = i.ToString()
+            };
+            virtualDbList.Add(w);
+            for (int j = 1; j < 3; j++)
+            {
+                id++;
+                Model x = new()
+                {
+                    Id = id,
+                    ParentId = w.Id,
+                    Name = $"{i}.{j}"
+                };
+                virtualDbList.Add(x);
+                for (int k = 1; k < 3; k++)
+                {
+                    id++;
+                    Model y = new()
+                    {
+                        Id = id,
+                        ParentId = x.Id,
+                        Name = $"{i}.{j}.{k}"
+                    };
+                    virtualDbList.Add(y);
+                    for (int l = 1; l < 3; l++)
+                    {
+                        id++;
+                        Model z = new()
+                        {
+                            Id = id,
+                            ParentId = y.Id,
+                            Name = $"{i}.{j}.{k}.{l}"
+                        };
+                        virtualDbList.Add(z);
+                    }
+                }
+            }
+        }
+        return virtualDbList.OrderBy(x => x.ParentId).ThenBy(x => x.Id).ToList();
+    }
+}
--- Recursive/Recursive/Program.cs
+++ Recursive/Recursive/Program.cs
@@ -1,9 +1,94 @@
-﻿namespace Recursive;
+﻿using Recursive.Models.CSharpModels;
+using Recursive.Models.ViewModels;
+using System.Text.Json;
+
+namespace Recursive;
 
 internal class Program
 {
+    static List<Model> BuildModelTreeFromOrigin(List<Model> modelList)
+    {
+        foreach (var item in modelList)
+        {
+            item.Children = modelList.Where(x => x.ParentId == item.Id).ToList();
+            ViewModel viewModel = new();
+            foreach (var i in item.Children)
+            {
+                RecursiveBuildModel(item.Children, i);
+            }
+        }
+        return modelList;
+    }
     static void Main(string[] args)
     {
-        
+        List<Model> virtualDbList = Model.CreateModelList();
+        List<Model> parentList = virtualDbList.Where(x => x.ParentId == null).ToList();
+
+        virtualDbList = BuildModelTreeFromOrigin(virtualDbList);
+
+        Console.WriteLine("BuildViewModelsFromParent:");
+        Console.WriteLine(JsonSerializer.Serialize<List<ViewModel>>(BuildViewModelsFromParent(parentList), new JsonSerializerOptions { WriteIndented = true }));
+
+        Console.WriteLine("\nBuildViewModelsFromFirstChild");
+        Console.WriteLine(JsonSerializer.Serialize<List<ParentViewModel>>(BuildViewModelsFromFirstChild(parentList), new JsonSerializerOptions { WriteIndented = true }));
+    }
+
+    //if parent is different than subelement
+    static List<ParentViewModel> BuildViewModelsFromFirstChild(List<Model> parentList)
+    {
+        List<ParentViewModel> viewList = new();
+        foreach (Model dr in parentList)
+        {
+            ParentViewModel root = new() { Id = dr.Id, Name = dr.Name };
+            foreach (var drChild in dr.Children)
+            {
+                ViewModel rootChild = new() { Id = drChild.Id, Pid = drChild.ParentId, Name = drChild.Name };
+                root.SubList.Add(rootChild);
+                RecursiveBuildViews(rootChild, drChild, root.SubList);
+            }
+            viewList.Add(root);
+        }
+        return viewList;
+    }
+
+    static List<ViewModel> BuildViewModelsFromParent(List<Model> parentList)
+    {
+        List<ViewModel> viewList = new();
+
+        foreach (Model dr in parentList)
+        {
+            ViewModel root = new() { Id = dr.Id, Pid = dr.ParentId, Name = dr.Name };
+            viewList.Add(root);
+            RecursiveBuildViews(root, dr, viewList);
+        }
+        return viewList;
+    }
+
+    static void RecursiveBuildViews(ViewModel root, Model model, List<ViewModel> viewList)
+    {
+        if (model.Children?.Count > 0)
+        {
+            foreach (var dr in model.Children)
+            {
+                ViewModel child = new() { Id = dr.Id, Pid = dr.ParentId, Name = dr.Name };
+                root.SubList.Add(child);
+                RecursiveBuildViews(child, dr, viewList);
+            }
+        }
+    }
+
+    static void RecursiveBuildModel(List<Model> source, Model buildNode)
+    {
+        List<Model> newSource = source.Where(a => a.Id == buildNode.ParentId).ToList();
+        foreach (var i in newSource)
+        {
+            Model newNode = new() { Id = i.Id, ParentId = i.ParentId, Name = i.Name };
+
+            if (buildNode is not null)
+            {
+                buildNode.Children.Add(newNode);
+            }
+            RecursiveBuildModel(source, newNode);
+        }
     }
 }
